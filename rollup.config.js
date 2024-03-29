d499c35c42366a7b06cade51e350336f89de3d29
Vanya Usalko <dk4aol@gmail.com>
2024-03-29 06:06:16 +0300

    # There is the fix for an compile issue. (node:1364266) Warning: To load an ES module, set "type": "module" in the package.json or use the .mjs extension. (Use `node --trace-warnings ...` to show where the warning was created) [!] RollupError: Node tried to load your configuration file as CommonJS even though it is likely an ES module. To resolve this, change the extension of your configuration to ".mjs", set "type": "module" in your package.json file or pass the "--bundleConfigAsCjs" flag.

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ import commonjs from '@rollup/plugin-commonjs'
 import typescript from '@rollup/plugin-typescript'
 import dts from 'rollup-plugin-dts'
 
-const packageJson = require('./package.json')
+import packageJson from './package.json' with { type: 'json' }
 
 export default [
   {
